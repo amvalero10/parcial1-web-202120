b986bdaab8ab152f600df89111831c97c7aae105
Angelo Marcetty <angelomarcetty@iMac-de-Fernanda.local>
2021-10-01 14:19:54 -0500

    para release

--- README.md
+++ README.md
@@ -17,5 +17,12 @@
 5. Ir a **http://localhost:3000** para ver la pantalla inicial
 
 ## Documentación endpoint
+Al cargar la pagina para buscar la altura de los jugadores de NBA, la carga genera la tabla y los inputs en blanco.
 
-Agregue aquí al documentación del endpoint elaborado.
+Al ingresar un numero de altura de jugador, el submit actva una funcion en el codigo de script.js, aqui se captura el input con el valor numerico referente a la suma de las alturas.
+
+Luego se llama a una funcion llamada callApi() la cual recibe como parametro el valor del input. Esta funcion realiza un fecth a la pagina para descargar los datos de los jugadores, recibe los datos en json y luego envia estos datos a otra funcion llamada pintarTabla, la cual recibe como argumento los datos de los jugadores y el numero del input de las alturas.
+
+pintarTabla selecciona el id en el dom del objeto 'table', primero lo limpia y luego realiza un dos forEach para comparar las alturas de los jugadores. Si la suma de las alturas coicide, se agrega en la tabla las filas correspodientes a los nombres de los jugadores. La funcion de CreateElemente permite crear una nueva fila con los parametros ingresados y la funcion append agrega esta fila a la tabla.
+
+Finalmente las funciones por medio de la estructura del DOM pintan la tabla con los jugadores cuyas sumas son iguales al input recibido.
--- public/script.js
+++ public/script.js
@@ -5,12 +5,19 @@ const table = document.getElementById('table');
 const tbody = document.getElementById('body-table');
 const message = document.getElementById('message');
 
+
+
 enterButton.addEventListener('click', (event) => {
-  //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getresults(123);
+
+  var numeroP = input.value
+
+  callApi(numeroP)
+
+  //getresults(123);
   event.preventDefault();
 });
+
+
 
 /**
  * Llamado al backend con queryParam
@@ -19,12 +26,88 @@ enterButton.addEventListener('click', (event) => {
 async function getresults(heightRef) {
   const resp = await fetch(`api?input=${heightRef}`);
   const data = await resp.json();
-  console.log('data from back', data);
+  //console.log(data);
   //printValues(data);
 }
+
+
 
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
 }
+
+
+
+
+
+function callApi(pNumero) {
+  fetch(
+    `https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players`
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      //console.log('data', data);
+      //console.log(data.values);
+     //return data.values
+     pintarTabla(data.values,pNumero)
+     // getresults(data.values)
+
+    });
+}
+
+
+
+
+ function pintarTabla(datos,pNumero){
+  console.log(datos);
+
+
+
+
+
+  pNumero = parseInt(pNumero)
+
+  const tablaCont = document.getElementById('table');
+  var indice = 1
+
+  tablaCont.innerHTML = "";
+  tablaCont.innerHTML = ` <tr>
+  <th scope="col">#</th>
+  <th scope="col">First Player</th>
+  <th scope="col">Second Player</th>
+</tr>`;
+ 
+
+  //extraer y ordernar
+
+  datos.forEach(a => {
+  datos.forEach(b => {
+   var aAlt = parseInt(a.h_in)
+   var bAlt = parseInt(b.h_in)
+   var suma = parseInt(aAlt + bAlt)
+
+    //console.log("a",aAlt)
+    //console.log("b",bAlt)
+    if(suma === pNumero){
+     //console.log("suma de los numeros",suma)
+     //console.log("valor p",pNumero) 
+     const linea = document.createElement('tr');
+     linea.innerHTML = `        
+      <td >${indice}</td>
+      <td >${a.first_name} ${a.last_name}</td>  
+      <td >${b.first_name} ${b.last_name}</td> 
+                               `;
+     tablaCont.append(linea)
+     indice = indice +1
+    }  
+  }); 
+});
+
+}
+
+
+
+
+
